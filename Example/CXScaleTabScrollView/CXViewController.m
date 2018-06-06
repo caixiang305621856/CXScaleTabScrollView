648f092524921ab05376b0499c18b6dce8e03a7d
caixiang <yanyan305621856@sina.com>
2018-06-06 21:37:22 +0800

    防简书的一次个人中心页的尝试

--- Example/CXScaleTabScrollView/CXViewController.m
+++ Example/CXScaleTabScrollView/CXViewController.m
@@ -7,21 +7,47 @@
 //
 
 #import "CXViewController.h"
+#import "ZHSegmentViewController.h"
+#import "UIView+ZHSegmentPostion.h"
 
 @interface CXViewController ()
+
+@property (nonatomic,strong)ZHSegmentViewController *segmentViewController;
 
 @end
 
 @implementation CXViewController
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+- (ZHSegmentViewController *)segmentViewController{
+    if (!_segmentViewController) {
+        ZHSegmentViewController *segmentViewController = [[ZHSegmentViewController alloc] init];
+        [self addChildViewController:segmentViewController];
+        _segmentViewController = segmentViewController;
+    }
+    return _segmentViewController;
 }
 
-- (void)didReceiveMemoryWarning
-{
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.segmentViewController.segmentBar.frame = CGRectMake(0, 0, self.view.width, 50);
+    self.segmentViewController.view.frame = CGRectMake(0, self.segmentViewController.segmentBar.height, self.view.width, self.view.height - self.segmentViewController.segmentBar.height);
+    [self.view addSubview:self.segmentViewController.view];
+    [self.view addSubview:self.segmentViewController.segmentBar];
+    
+    NSArray *items = @[@"标签1", @"标签2", @"标签3"];
+    UIViewController *vc1 = [UIViewController new];
+    vc1.view.backgroundColor = [UIColor colorWithRed:0.419 green:0.781 blue:0.977 alpha:1.000];
+    UIViewController *vc2 = [UIViewController new];
+    vc2.view.backgroundColor = [UIColor colorWithRed:0.957 green:0.557 blue:0.231 alpha:1.000];
+    UIViewController *vc3 = [UIViewController new];
+    vc3.view.backgroundColor = [UIColor colorWithWhite:0.729 alpha:1.000];
+    [self.segmentViewController setUpWithItems:items childViewControllers:@[vc1,vc2,vc3]];
+    [self.segmentViewController.segmentBar updateWithConfig:^(ZHSegmentBarConfig *config) {
+        config.itemSelectColor = [UIColor yellowColor];
+    }];
+}
+
+- (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
